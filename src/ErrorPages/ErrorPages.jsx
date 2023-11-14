05395f740de9f46aee791a2e94504af715904f17
mdrabiulis <mdrabiulis155@gmail.com>
2023-11-15 00:06:17 +0600

    errorpages

--- src/ErrorPages/ErrorPages.jsx
+++ src/ErrorPages/ErrorPages.jsx
@@ -1,9 +1,12 @@
-
+import { Link } from "react-router-dom";
+import errorimg from "/public/ErrorPage404-03.jpg"
 
 const ErrorPages = () => {
     return (
-        <div>
-            ErrorPages
+        
+        <div className=" flex flex-col items-center">
+            <Link to={"/"}><button className="bg-[#6C53F8] h-12 text-white rounded-md text-2xl mt-3 w-44">Back to Home</button></Link>
+            <img src={errorimg} alt="" />
         </div>
     );
 };
